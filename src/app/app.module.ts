40dcac5e8f6f0d2fc14abb8b648a16880a2179ca
darkosika <osmnkpln03ozgurluk@gmail.com>
2021-01-02 20:08:19 +0200

    save expense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router'
+import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
+import { ListExpensesComponent } from './components/list-expenses/list-expenses.component';
+import {HttpClientModule} from '@angular/common/http';
+import { AddExpenseComponent } from './components/add-expense/add-expense.component'
 
+const routers : Routes = [
+  {path : 'expenses', component : ListExpensesComponent},
+  {path : 'addexpense', component : AddExpenseComponent},
+  {path : '', redirectTo : '/expenses',pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListExpensesComponent,
+    AddExpenseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routers)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/add-expense/add-expense.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, Routes } from '@angular/router';
+import { Expense } from 'src/app/models/expense';
+import { ExpenseService } from 'src/app/services/expense.service';
+
+@Component({
+  selector: 'app-add-expense',
+  templateUrl: './add-expense.component.html',
+  styleUrls: ['./add-expense.component.css']
+})
+export class AddExpenseComponent implements OnInit {
+
+  expense : Expense = new Expense()
+
+  constructor(private expenseService : ExpenseService, private _router : Router) { }
+
+  ngOnInit(): void {
+  }
+  
+  saveExpense () {
+      this.expenseService.saveExpense(this.expense).subscribe(
+        data => {
+          console.log('response', data)
+          this._router.navigateByUrl("/expenses")
+        }
+      )
+  }
+}
